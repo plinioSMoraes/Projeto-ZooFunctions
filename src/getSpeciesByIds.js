82a9879d65c61aac7d82733b7e36ad159ab01991
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-06-29 13:37:53 -0500

    Requisito 1 - Feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  
+function getSpeciesByIds(...ids) {
+  const speciesIds = [];
+  ids.forEach((id) => {
+    data.species.forEach((species) => {
+      if (species.id === id) {
+        speciesIds.push(species);
+      }
+    });
+  });
+  return speciesIds;
 }
 
 module.exports = getSpeciesByIds;
