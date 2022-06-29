bc07ea86b5420025f5ad6fd9cc01a40ae223def3
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-06-29 14:05:56 -0500

    Requisito 2 - Resolvido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  let statement = '';
+  data.species.forEach((species) => {
+    if (species.name === animal) {
+      statement = species.residents.every((resident) => (resident.age >= age));
+    }
+  });
+  return statement;
 }
+console.log(getAnimalsOlderThan('lions', 7));
 
 module.exports = getAnimalsOlderThan;
