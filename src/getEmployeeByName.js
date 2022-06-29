8fdb6aef4e900604a31f9a343506ddbac5cfbcc1
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-06-29 14:19:08 -0500

    Requisito 3 - Feito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  let employ = {};
+  data.employees.forEach((employees) => {
+    if (employees.firstName === employeeName || employees.lastName === employeeName) {
+      employ = employees;
+    }
+  });
+  return employ;
 }
-
+console.log(getEmployeeByName('Bethea'));
 module.exports = getEmployeeByName;
