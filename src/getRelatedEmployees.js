022d17837f14911b2a1b0871882fcde4324a75d8
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-06-29 15:55:54 -0500

    Requisito 4 - Feito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  let statement = false;
+  data.employees.forEach((employe) => {
+    employe.managers.forEach((manager) => {
+      if (manager === id) {
+        statement = true;
+      }
+    });
+  });
+  return statement;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const managedEmployees = [];
+    data.employees.forEach((employe) => {
+      if (employe.managers.includes(managerId)) {
+        const name = `${employe.firstName} ${employe.lastName}`;
+        managedEmployees.push(name);
+      }
+    });
+    return managedEmployees;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
 module.exports = { isManager, getRelatedEmployees };
