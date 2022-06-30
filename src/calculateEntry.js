55ff2a703ba751c00f76db3550267966bac5bef4
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-06-29 22:56:27 -0500

    Requisito 8 - Lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,14 +1,5 @@
 const data = require('../data/zoo_data');
 
-const entradas = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
 function countEntrants(entrants) {
   const pessoas = { child: 0, adult: 0, senior: 0 };
   entrants.forEach((entrant) => {
@@ -27,6 +18,5 @@ function calculateEntry(entrants) {
   const pessoas = countEntrants(entrants);
   return pessoas.child * 20.99 + pessoas.adult * 49.99 + pessoas.senior * 24.99
 }
-// console.log(countEntrants(entradas));
-console.log(calculateEntry({}));
+
 module.exports = { calculateEntry, countEntrants };
