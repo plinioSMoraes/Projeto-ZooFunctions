3886a145fe1ec138ce3fde94f9673e6f7c1dbaa1
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-06-29 20:42:20 -0500

    Requisito 5 - Feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
+function filterAllAnimals(animal) {
+  const objects = {};
+  data.species.forEach((specie) => {
+    objects[specie.name] = specie.residents.length;
+  });
+  return objects;
+}
+function animalsByName(animal, allAnimals) {
+  const statement = Object.prototype.hasOwnProperty.call(allAnimals, animal.specie);
+  if (statement) return allAnimals[animal.specie];
+}
+function animalsBySex(animal) {
+  let sexCounts = 0;
+  data.species.forEach((specie) => {
+    specie.residents.forEach((resident) => {
+      if (specie.name === animal.specie && resident.sex === animal.sex) sexCounts += 1;
+    });
+  });
+  return sexCounts;
+}
 function countAnimals(animal) {
-  // seu código aqui
+  const allAnimals = filterAllAnimals(animal);
+  if (!animal) return allAnimals; // retorna a todas especies e suas quantidades
+  if (animal.specie && !animal.sex) return animalsByName(animal, allAnimals); // retorna a quantidade de animais por especie
+  return animalsBySex(animal); // contas os animais por sexo
 }
 
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 module.exports = countAnimals;
