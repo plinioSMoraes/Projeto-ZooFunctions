f9ad08ad5728ad5e7229f1f1385a95ce3abdeace
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-06-29 23:33:53 -0500

    Requisito 10 - Feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+function oldestAnimal(animalId) {
+  let age = 0;
+  let animalObj = [];
+  data.species.forEach((specie) => {
+    if (specie.id === animalId) {
+      Object.values(specie.residents).forEach((eachAnimal) => {
+        if (eachAnimal.age > age) {
+          animalObj = Object.values(eachAnimal);
+          age = eachAnimal.age;
+        }
+      });
+    }
+  });
+  return animalObj;
+}
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  let oldAnimal = [];
+  data.employees.forEach((index) => {
+    if (index.id === id) {
+       oldAnimal = oldestAnimal(index.responsibleFor[0]);
+    }
+  });
+  return oldAnimal;
 }
 
 module.exports = getOldestFromFirstSpecies;
